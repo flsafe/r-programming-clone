1c8d3094afdc3a6202722a7ab41d910c852c1bc7
frank licea <flicea@jetsauce.local>
2010-02-05 18:55:39 -0700

    Adding initial red up/down vote arrows. It's kind of slow, I may need to change the way this works.

--- views/elements/vote.ctp
+++ views/elements/vote.ctp
@@ -1,10 +1,10 @@
 <?php #params id ?>
 <div class="vote">
-	<a href="#" id="<?php echo $id ?>" class="upvote" onClick="return false;">
-		<img src="/img/up_arrow.gif" width="16" height="16"/>
+	<a href="#" id="<?php echo "upvote${id}" ?>" class="upvote" onClick="return false;">
+		<img id="<?php echo "upvoteimg${id}" ?>"src="/img/up_arrow.gif" width="16" height="16"/>
 	</a> 
 	
-	<a href="#" id="<?php echo $id ?>"onClick="return false;" class="downvote">
-		<img src="/img/down_arrow.gif" width="16" height="16"/>
+	<a href="#" id="<?php echo "downvote{$id}" ?>" class="downvote" onClick="return false;">
+		<img id="<?php echo "downvoteimg${id}" ?>" src="/img/down_arrow.gif" width="16" height="16"/>
 	</a>
 </div class="vote">
--- webroot/js/jquery/vote.js
+++ webroot/js/jquery/vote.js
@@ -1,39 +1,44 @@
 
-$(document).ready(function(){
+function vote(cntrl, type, id){
+    $.post('/' + cntrl + '/vote/' + type + '/' + id, function(data){
 	
+    		try{
+    			var resp = JSON.parse(data);
+    			if(resp.points == null)
+    				return;/*Some kind of server error*/
+
+    			$('#points'+id ).html(resp.points + ' points |');
+
+    			var src;
+    			var imgid;
+    			if(type == "up"){
+			        src = '/img/up_arrow_red.gif';
+			        imgid = '#upvoteimg';
+			    }
+    			else{
+    			    src = '/img/down_arrow_red.gif';
+    			    imgid = '#downvoteimg'
+			    }
+			    imgid += id;
+			    
+			    $('#upvoteimg'+id).attr('src','/img/up_arrow.gif');
+			    $('#downvoteimg'+id).attr('src','/img/down_arrow.gif');
+			    $(imgid).attr('src', src);
+    		}
+    		catch(err){/*Empty string is returned if user is not logged in*/
+    		    window.location = "/users/login";
+    		}});
+}
+
+
+$(document).ready(function(){
 	$('.upvote').click(function(){
-		var topicid = $(this).attr('id');
-
-		$.post('/'+controller+'/vote/up/' + topicid, function(data){
-			
-				try{
-					var resp = JSON.parse(data);
-					if(resp.points == null)
-						return;
-
-					$('#points'+topicid ).html(resp.points + ' points |');
-				}
-				catch(err){/*Empty string is returned if user is not logged in*/
-				    window.location = "/users/login";
-				}
-			});
-	});
+		var topicid = $(this).attr('id').replace("upvote","");
+        vote(controller,"up",topicid);
+        });
 	
 	$('.downvote').click(function(){
-		var topicid = $(this).attr('id');
-
-		$.post('/'+controller+'/vote/down/' + topicid, function(data){
-			
-				try{
-					var resp = JSON.parse(data);
-					if(resp.points == null)
-						return;
-
-					$('#points' + topicid ).html(resp.points + ' points |');
-				}
-				catch(err){
-                    window.location = '/users/login';
-				}
-			});
-	});
+		var topicid = $(this).attr('id').replace("downvote","");
+        vote(controller,"down",topicid);
+        });
 });
