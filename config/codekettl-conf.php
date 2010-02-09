cc0e2e1535914d1d998f7c3dd27ada97a84ae136
Francisco Licea <flicea@elise.(none)>
2010-02-08 20:04:38 -0700

    Commiting fix for issue7. Added time based ranking to the topics. Still need to add them to submissions

--- config/codekettl-conf.php
+++ config/codekettl-conf.php
@@ -1,3 +1,5 @@
 <?php
-	$config['company.name'] = "CozySystems LLC";
+$config['company.name'] = 'CozySystems LLC';
+$config['codekettl.ranking.c'] = '1.5';
+$config['codekettl.ranking.b'] = '2.0';
 ?>
--- models/vote.php
+++ models/vote.php
@@ -11,12 +11,13 @@ class Vote extends AppModel{
 	*voted on. Must have a public name field.
 	*@param string $model_id The id of the model to be voted on
 	*@param string $user_id The id of the user that is logged in
+  *@pram function $rankFunction The function to use to rank the model object.
 	*@return integer Returns (upvotes - downvotes) if the user hasn't voted
 	*on the model before or changes his vote.
 	*Otherwise returns null if the user vote doesn't take effect, the vote type isn't
 	*'up' or 'down' or there are not database objects with the ids provided.
 	*/
-	public function voteForModel($type, $model, $model_id, $user_id){
+	public function voteForModel($type = null, $model = null, $model_id = null, $user_id = null, $rankFunction = array('self','hackerNewsRank')){
 		if($type != 'up' && $type != 'down')
 			return;
 		$modelname = $model->name;
@@ -45,13 +46,16 @@ class Vote extends AppModel{
 					$modeldata[$modelname]['upvotes']--;
 				}
 				
-				#TODO: Calc rank based on time
-				$modeldata[$modelname]['rank'] = $modeldata[$modelname]['upvotes'] - $modeldata[$modelname]['downvotes'];
-				
+        $upvotes   = $modeldata[$modelname]['upvotes'];
+        $downvotes = $modeldata[$modelname]['downvotes'];
+        $created   = $modeldata[$modelname]['created'];
+        $rank      = call_user_func($rankFunction, $upvotes, $downvotes, $created);
+				$modeldata[$modelname]['rank'] = $rank;
+
 				$model->save($modeldata);
 				$this->save($votedata);
 				
-				return $modeldata[$modelname]['upvotes'] - $modeldata[$modelname]['downvotes'];
+				return $upvotes - $downvotes;
 			}
 		}
 		else{
@@ -64,10 +68,13 @@ class Vote extends AppModel{
 			else
 				$down = $modeldata[$modelname]['downvotes']++;
 				
-			#TODO: Calculate rank based on time
-			$modeldata[$modelname]['rank'] = $modeldata[$modelname]['upvotes'] - $modeldata[$modelname]['downvotes'];
+      $upvotes = $modeldata[$modelname]['upvotes'];
+      $downvotes = $modeldata[$modelname]['downvotes'];
+      $created = $modeldata[$modelname]['created'];
+      $rank = call_user_func($rankFunction, $upvotes, $downvotes, $created);
+      $modeldata[$modelname]['rank'] = $rank;
+
 			$model->save($modeldata);
-			
 			$modelnamelow = strtolower($modelname);
 			$votedata = array('Vote' =>array(
 					'user_id'            => $user_id,
@@ -75,19 +82,19 @@ class Vote extends AppModel{
 					'upvote'             => $upvote,));
 			$this->save($votedata);
 			
-			return $modeldata[$modelname]['upvotes'] - $modeldata[$modelname]['downvotes'];
+			return $upvotes - $downvotes;
 		}
 	}
 	
 	/**
 	*Returns an associative array mapping model ids to 
 	*the user vote for that model.
-	*@param $model The object to get the votes for. Must have a public name field.
+	*@param $modelname The name of the model. Typicaly the Model->name field.
 	*@param array $modelids An array of model ids. The function returns the user vote,
 	*if a vote exists, for each model id.
-	*@param string $userid The user to get the votes for
+	*@param string $userid The user to get the votes for.
 	*@return Returns an associative array such that: array[modelid] => "1" or "0".
-	*"1" is returned if the user upvoted that model, otherwise "0". If the user did not
+	*"1" is returned if the user upvoted that model, otherwise "0" if down voted. If the user did not
 	*vote on the model at all then array[modelid] will not be set.
 	*/
 	public function getUserVotes($modelname, $modelids, $userid){
@@ -106,5 +113,31 @@ class Vote extends AppModel{
 		}
 		return $userVotesTab;
 	}
+
+  /**
+   *Returns the HackerNews rank based on upvotes - downvotes and hours
+   *since created. Nothing stays on the front page more than a few hours.
+   *@param numeric $upvotes The number of upvotes
+   *@param numeric $downvotes The number of downvotes
+   *@param DateTime string $created The datetime string indicating when
+   *the object getting the rank was created. example: 2010-02-08 12:09:30
+   *@return Returns the rank
+   */
+  static function hackerNewsRank($upvotes, $downvotes, $created){
+    $secondsPerHour = 3600;
+    $points         = $upvotes - $downvotes;
+    $c              = Configure::read('codekettl.ranking.c');
+    $b              = Configure::read('codekettl.ranking.b');
+
+    $datetime       = new DateTime($created);
+    $created        = $datetime->format('U');
+    $now            = new DateTime();
+    $now            = $now->format('U');
+
+    $ageInHours     = ($now - $created) / $secondsPerHour;
+
+    $rank           =  ($points - 1)/pow(($ageInHours + $b), $c);
+    return $rank;
+  }
 }
 ?>
