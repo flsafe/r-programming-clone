fb704a2f21dd7e23da1682bae6f3c39b4b7ec98f
Francisco Licea <francisco.licea@gmail.com>
2010-03-18 21:06:21 -0600

    Adding clear cache on save to app model

--- models/app_model.php
+++ models/app_model.php
@@ -1,7 +1,14 @@
 <?php
+App::import('Core', 'Cache');
 class AppModel extends Model{
 		function checkCaptcha($check, $modelname){
 			return $check['captcha'] == $this->data[$modelname]['captcha_keystring'];
+		}
+		
+		public function afterSave(){
+			#Clear view cache for this object, and any vote data for this object
+			Cache::delete("{$this->name}.{$this->id}", 'default');
+			Cache::delete("Vote.{$this->name}.{$this->id}");
 		}
 }
 ?>
